637944b850f06fab85061b1e748f1bd06923cc9a
Gustavo Toffetti <gustavotoffetti@MacBook-Air-de-Gustavo.local>
2021-10-26 00:44:17 -0300

    First commit with exercises from Aula4 - Front-end, and exercises from Aula4 - Programação Imperativa.

---
+++ ProgramacaoImperativa/aula4/ImcAula4.js
@@ -0,0 +1,9 @@
+let nome = 'Carlos';
+let sobrenome = 'de Souza';
+let idade = 28;
+let peso = 80.1;
+let altura = 1.76;
+let plano = 'Diamante'
+let imc = peso / (altura ** altura)
+
+console.log(nome + ' ' + sobrenome + ' tem ' + idade + ' anos,' + ' possui o plano ' + plano + ' e seu índice de massa corporal de ' + imc)
---
+++ ProgramacaoImperativa/aula4/exAula4.js
@@ -0,0 +1,18 @@
+console.log(2+2); // soma
+console.log(2-1); // subtracao
+
+let x = 10;
+let y = 9;
+console.log(x+y); // soma
+let soma = x+y;
+console.log(soma);
+
+
+let a = 'Olá!';
+let b = 23;
+console.log(a+b);
+
+let c = 'Eu tenho';
+let d = '25';
+let e = 'anos.';
+console.log(c+' '+d+' '+e);
