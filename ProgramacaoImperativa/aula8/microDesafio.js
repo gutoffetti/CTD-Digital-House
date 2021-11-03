914e07ad8c7387de72fcad417c465e0d30eedc0d
Gustavo Toffetti <gustavotoffetti@MacBook-Air-de-Gustavo.local>
2021-11-03 20:58:42 -0300

    Resolução do exercício(Micro Desafio) do Playground, referente a Aula 8.

---
+++ ProgramacaoImperativa/aula8/microDesafio.js
@@ -0,0 +1,22 @@
+// Micro Desafio 1:
+console.log("\n ---------- Micro Desafio 1 ---------- \n")
+var status = true;
+if (status == true) {
+    console.log("O valor é true - verdadeiro. \n")
+}
+else {
+    console.log("O valor é false - falso. \n")
+}
+
+
+// Micro Desafio 2:
+console.log("---------- Micro Desafio 2 ---------- \n")
+var linguagem = "Português";
+if (linguagem == "javascript") {
+    console.log("Estou aprendendo! \n")
+}
+else {
+    console.log("Aprenderei mais tarde. \n")
+}
+
+
